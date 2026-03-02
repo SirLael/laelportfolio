9c92283840e3408684aa3324c9b7e70b41682bd3
SirLael <brianlaelsilva@gmail.com>
2026-03-02 00:57:14 -0300

    Changed navbar text

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,7 +33,7 @@ export function Navbar() {
     >
       <nav className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
         <a href="#" className="font-mono text-sm tracking-wider text-primary">
-          {"<Lael's Portfolio />"}
+          {"<Lael Developer/>"}
         </a>
 
         {/* Desktop */}
